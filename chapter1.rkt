c244c5ea18ef8d46dcd313a066eb9f698acb1681
Alex Bogart <alex@bitlimn.com>
2012-08-03 19:12:17 -0400

    Chapter 1 1.1 - 1.7 (partial 1.8)
    Starting off easy. Let's do this!

--- chapter1.rkt
+++ chapter1.rkt
@@ -1,1 +1,124 @@
 #lang planet neil/sicp
+
+;ex 1.1
+
+;10
+;12
+;8
+;3
+;6
+;N/A
+;N/A
+;19
+;#f
+;4
+;16
+;6
+;16
+
+;ex 1.2
+
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+   (* 3 (- 6 2) (- 2 7)))
+
+;ex 1.3
+
+;commentary: this exercise does not specify for cases where two numbers are
+;equal. I'll assume that it's not in the function domain.
+
+(define (sum-of-squares a b)
+  (+ (* a a) (* b b)))
+
+(define (sum-of-max-squares a b c) 
+  ;we'll need two compares
+  (if (> a b)
+      (if (> b c)
+          ;c is smallest
+          (sum-of-squares a b)
+          ;b is smallest
+          (sum-of-squares a c))
+      (if (> a c)
+          ;c is smallest
+          (sum-of-squares a b)
+          ;a is smallest
+          (sum-of-squares b c))))
+
+;ex 1.4
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;the `if` statement in this function will return a function depending on the
+;conditional. Because of this, the `if` statement appears in the statement
+;surrounding it as a function/verb itself.
+
+;ex 1.5
+
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+         0
+         y))
+
+;scheme will recurse over `p` indefinitely (until all the stack space is gone).
+;This is because it is applicative-order; it evaluates the operator (the first
+;value in the s-expression) and the operands (the other values in the s-exp)
+;before applying the operator to the operands.
+
+;In this case, scheme evaluates (p) before applying `test`, and `p` never
+;returns.
+
+;ex 1.6
+
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess) x)) 0.001))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (new-if pred then-cl else-cl)
+  (cond (pred then-cl)
+        (else else-cl)))
+
+(define (sqrt-iter-new guess x)
+  (new-if (good-enough? guess x)
+      guess
+      (sqrt-iter-new (improve guess x) x)))
+
+;when Alyssa attempts to run `sqrt-iter-new`, it will hang. `new-if` is
+;applicative order, so it will evaluate *both* the then and else-clause, instead
+;of only evaluating one of them. Because of this, the recursion will never end.
+
+;ex 1.7
+
+;`sqrt-iter` will fail on both small and large numbers. For small numbers,
+;`good-enough?` is too easily satisfied. For large numbers, `good-enough?` will
+;be too stingy and won't ever be satisfied.
+
+(define (square x)
+  (* x x))
+
+(define (good-enough-2? guess x last-guess)
+  ;if they are within 5% of each other.
+  (< (abs (- (/ guess last-guess) 1)) 0.05))
+
+(define (sqrt-iter-helper guess x last-guess)
+  (if (good-enough-2? guess x last-guess)
+      guess
+      (sqrt-iter-helper (improve guess x) x guess)))
+
+(define (sqrt-iter-2 guess x)
+  (sqrt-iter-helper guess x 10000000))
+
+;ex 1.8
+
+;cube root approximator using the `good-enough-2?` method from ex 1.7.
