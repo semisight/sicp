c208d137d6c33484e1e394cc318ed464d197b7ad
Alex Bogart <alex@bitlimn.com>
2012-08-10 17:24:35 -0400

    Adds 1.21.

--- chapter1.rkt
+++ chapter1.rkt
@@ -391,3 +391,20 @@
 ;final call to gcd, adding an additional 4 calls to `remainder` in the end.
 
 ;ex 1.21
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (inc test-divisor)))))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+;    (smallest-divisor 199): 199
+;    (smallest-divisor 1999): 1999
+;    (smallest-divisor 19999): 7
+
+;ex 1.22
