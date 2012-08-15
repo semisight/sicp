4f043e77857b8ad5a47f9bd1aab0ca254f69318d
Alex Bogart <alex@bitlimn.com>
2012-08-14 23:14:41 -0700

    test.py now generates html files.

--- test.py
+++ test.py
@@ -1,24 +1,48 @@
 #!/usr/bin/env python
-from __future__ import print_function
-import sys, json
+import os, re, json
+from markdown2 import markdown as md
 
 #TODO:
 #1. Translate to javascript.
 #2. Fetch input from github.com/semisight/sicp.
 #3. Put on bitlimn under examples.
 
-def parse_ex(acc, line):
-	if len(acc) == 0 and line[0:4] != ';ex ':
-		return []
+def handle_line(line):
+	result = line
 
-	if line[0:4] == ';ex ':
-		acc.append([line])
+	if len(line) == 0:
+		return result
+
+	if line[0] == ';':
+		result = line[1:]
 	else:
-		acc[-1].append(line)
+		result = '    ' + line
 
-	return acc
+	return result
 
-with open(sys.argv[1]) as f:
-	inp = f.readlines()
+def handle_ex(ex):
+	result = [handle_line(x) for x in (';##' + ex).splitlines()]
 
-print(json.dumps(reduce(parse_ex, inp, []), indent=2))
+	return '\n'.join(result)
+
+def handle_file(val):
+	result = re.split(';ex ', val)
+	result = [md(handle_ex(x)) for x in result]
+
+	return '\n'.join(result)
+
+source = {}
+
+for (path, _, files) in os.walk('.'):
+	for name in files:
+		if os.path.splitext(name)[1] != '.rkt':
+			continue
+
+		with open(name) as f:
+			source[name] = f.read()
+
+for name, src in source.iteritems():
+	with open(name+'.html', 'w') as f:
+		f.write(handle_file(src))
+		print handle_file(src)
+
