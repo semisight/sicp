8d5013e48a1f1205dc8bd57310d10fd1a1ac5407
Alex Bogart <alex@bitlimn.com>
2012-08-14 20:51:51 -0700

    Adds 1.30.

--- chapter1p2.rkt
+++ chapter1p2.rkt
@@ -162,3 +162,12 @@
 ;returns approximations.
 
 ;ex 1.30
+
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+;ex 1.31
