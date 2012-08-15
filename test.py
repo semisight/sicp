8ab48b300635bbec71164e0f42f8aa965bb270e1
Alex Bogart <alex@bitlimn.com>
2012-08-14 23:21:18 -0700

    test.py also provides pure markdown.
    Use '-m' option to activate.

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-import os, re, json
+import sys, os, re, json
 from markdown2 import markdown as md
 
 #TODO:
 #1. Translate to javascript.
 #2. Fetch input from github.com/semisight/sicp.
 #3. Put on bitlimn under examples.
+
+eval_md = True
+
+if len(sys.argv) > 1 and sys.argv[1] == '-m':
+	eval_md = False
 
 def handle_line(line):
 	result = line
@@ -21,13 +26,16 @@ def handle_line(line):
 	return result
 
 def handle_ex(ex):
-	result = [handle_line(x) for x in (';##' + ex).splitlines()]
+	result = '\n'.join([handle_line(x) for x in (';##' + ex).splitlines()])
 
-	return '\n'.join(result)
+	if eval_md:
+		return md(result)
+	else:
+		return result
 
 def handle_file(val):
 	result = re.split(';ex ', val)
-	result = [md(handle_ex(x)) for x in result]
+	result = [handle_ex(x) for x in result]
 
 	return '\n'.join(result)
 
@@ -42,7 +50,12 @@ for (path, _, files) in os.walk('.'):
 			source[name] = f.read()
 
 for name, src in source.iteritems():
-	with open(name+'.html', 'w') as f:
+	if eval_md:
+		new_ext = '.html'
+	else:
+		new_ext = '.md'
+
+	with open(name + new_ext, 'w') as f:
 		f.write(handle_file(src))
 		print handle_file(src)
 
