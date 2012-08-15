28bbac3069e300eada680b9f4ba4e3beae53e9bb
Alex Bogart <alex@bitlimn.com>
2012-08-15 12:14:59 -0700

    Adds 1.40.

--- chapter1p2.rkt
+++ chapter1p2.rkt
@@ -338,3 +338,23 @@
   (cf-iter n d k))
 
 ;ex 1.40
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (cube x) (* a (square x)) (* b x) c)))
+
+;ex 1.41
